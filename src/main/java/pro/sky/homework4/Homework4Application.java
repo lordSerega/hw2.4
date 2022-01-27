c0c6388c3f17b1d3fa5acda583f2c488148d0fe8
Kaplin Sergey <kaplin@internet.ru>
2022-01-27 17:35:31 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/homework4/Homework4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework4Application.class, args);
+	}
+
+}
